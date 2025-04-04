229512577f02654b6e451b066b45b7b20acba9a2
Sameer Kumar <2006sameeer2009@gmail.com>
2025-04-04 18:43:20 +0530

    setup prisma database

--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,23 @@
 import express from "express";
 import env from "./config/dotenv";
 import logger from "./config/logger";
+import { PrismaClient } from "@prisma/client";
 
 const app = express();
 const PORT = env.PORT || 5501;
+const prisma = new PrismaClient();
 
 // Middleware
 app.use(express.json());
 
 // Routes
-app.get("/", (req, res) => {
-  res.send("Hello, TypeScript Server!");
+app.get("/", async (_req, res) => {
+  await prisma.user.create({
+    data: { name: "Alice", email: "alice@example.com" },
+  });
+
+  const users = await prisma.user.findMany();
+  res.json({ message: "Welcome to Shrika backend", users });
 });
 
 // Start Server
