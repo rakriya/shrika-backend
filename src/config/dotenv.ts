5ceb0f5aba8785da388d3dbd79934aa0b62700ea
Sameer Kumar <2006sameeer2009@gmail.com>
2025-04-04 18:44:36 +0530

    use .env instend of .env.development

--- src/config/dotenv.ts
+++ src/config/dotenv.ts
@@ -1,13 +1,9 @@
+/* eslint-disable no-process-env */
 import { config } from "dotenv";
 import { cleanEnv, port, str } from "envalid";
-import path from "path";
 
-config({
-  // eslint-disable-next-line no-process-env
-  path: path.join(__dirname, `../../.env.${process.env.NODE_ENV || "development"}`),
-});
+config();
 
-// eslint-disable-next-line no-process-env
 export const env = cleanEnv(process.env, {
   PORT: port(),
   NODE_ENV: str({
