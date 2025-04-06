6d1178357fa2b4e971857c05379557d0d4057863
Sameer Kumar <2006sameeer2009@gmail.com>
2025-04-06 13:52:49 +0530

    add endpoint to get societies data and socierty data by id

--- src/controllers/societyController.ts
+++ src/controllers/societyController.ts
@@ -4,13 +4,7 @@ import logger from "../config/logger";
 import createHttpError from "http-errors";
 const prisma = new PrismaClient();
 
-interface ff extends Request {
-  body: {
-    name: string;
-  };
-}
-
-export const createSociety = async (req: ff, res: Response, next: NextFunction) => {
+export const createSociety = async (req: Request, res: Response, next: NextFunction) => {
   try {
     const { name } = req.body;
 
@@ -36,5 +30,30 @@ export const createSociety = async (req: ff, res: Response, next: NextFunction) 
     res.status(201).json({ message: "Society created successfully", newSociety });
   } catch (error) {
     return next(error);
+  }
+};
+
+export const getSocieties = async (req: Request, res: Response) => {
+  try {
+    // Logic to fetch all societies from the database
+    const societies = await prisma.society.findMany();
+    res.status(200).json(societies);
+  } catch (error) {
+    res.status(500).json({ message: "Error fetching societies", error });
+  }
+};
+
+export const getSocietyById = async (req: Request, res: Response) => {
+  try {
+    const { id } = req.params;
+
+    // Logic to fetch a society by ID from the database
+    const society = await prisma.society.findUnique({ where: { id } });
+    if (!society) {
+      return res.status(404).json({ message: "Society not found" });
+    }
+    res.status(200).json(society);
+  } catch (error) {
+    res.status(500).json({ message: "Error fetching society", error });
   }
 };
--- src/routes/societyRoute.ts
+++ src/routes/societyRoute.ts
@@ -1,5 +1,5 @@
 import express, { RequestHandler } from "express";
-import { createSociety } from "../controllers/societyController";
+import { createSociety, getAllSocieties, getSocietyById } from "../controllers/societyController";
 import { societyCreateSchema } from "../joiSchemas/societySchema";
 import { validate } from "../middlewares/validate";
 import { commonUploader, validateAllFiles } from "../middlewares/multer";
@@ -9,12 +9,11 @@ const router = express.Router();
 router.post(
   "/",
   validate(societyCreateSchema),
-  commonUploader("fields", [
-    { name: "logo", maxCount: 2 },
-    { name: "photo", maxCount: 4 },
-  ]),
+  commonUploader("single", "logo", 1),
   validateAllFiles as unknown as RequestHandler,
   createSociety,
 );
+router.get("/", getAllSocieties);
+router.get("/:id", getSocietyById);
 
 export default router;
