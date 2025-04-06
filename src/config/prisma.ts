d3cfe0b031faafe0ec5ac9e7fa167e97da6caf11
Sameer Kumar <2006sameeer2009@gmail.com>
2025-04-06 13:54:25 +0530

    hide member password

--- src/config/prisma.ts
+++ src/config/prisma.ts
@@ -1,5 +1,11 @@
 import { PrismaClient } from "@prisma/client";
 
-const prisma = new PrismaClient();
+const prisma = new PrismaClient({
+  omit: {
+    member: {
+      password: true,
+    },
+  },
+});
 
 export default prisma; // Use this single instance in your app
